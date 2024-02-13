d0138340813f5fc2d883b08fef29d586a6f9c236
sylvain <sylvainmasclet24@gmail.com>
2024-02-13 17:16:43 +0200

    added comments for todo in scripts.js

--- ascents/static/scripts.js
+++ ascents/static/scripts.js
@@ -1,3 +1,6 @@
 function redirect_to_ascent(pk){
-    document.location.href="ascent_details/"+pk+"/";
+    document.location.href="ascent_details/"+pk+"/"; 
+    // TODO: using django's url system doesn't seem to work,
+    // and this looks like a security fault
+    // so better try to find how to make it work
 }
